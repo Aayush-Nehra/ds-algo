09b2a03ce3fd584098b0b7ed12dc7ec46ec9c9cc
Aayush-Nehra <iaayushnehra@gmail.com>
2024-09-11 21:04:38 +0530

    dutch national flag algorithm

--- Array/Medium/sort_colors.py
+++ Array/Medium/sort_colors.py
@@ -23,5 +23,24 @@ def sortColors(nums) -> None:
 
     print(nums)
 
-sortColors([2,0,2,1,1,0])
+def sortColors2(nums):
+    """Dutch national flag algorithm"""
+    size = len(nums)
+    low, mid, high = 0, 0, size-1
+
+    while mid <= high:
+        if nums[mid] == 0:
+            nums[low], nums[mid] = nums[mid], nums[low]
+            low += 1
+            mid += 1
+        elif nums[mid] == 1:
+            mid += 1
+        else:
+            nums[mid], nums[high] = nums[high], nums[mid]
+            high -= 1
+
+    print(nums)
+
+
+sortColors2([2,0,2,1,1,0,0,2,1,1,0])
     
