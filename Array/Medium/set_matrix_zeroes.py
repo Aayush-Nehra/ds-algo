3df17e482c99836574a03044ddaf993d7d4fff23
Aayush-Nehra <iaayushnehra@gmail.com>
2024-09-19 11:57:55 +0530

    optimal algorithm set matrix zeroes

--- Array/Medium/set_matrix_zeroes.py
+++ Array/Medium/set_matrix_zeroes.py
@@ -21,7 +21,8 @@ def setZeroes(matrix):
             matrix[i][column] = 0
 
 def setZeroes2(matrix):
-    """Optimal approach using representative element"""
+    """Brute force using representative element""" 
+    #TC = O(n**3)
     def make_row_z(row, n):
         for j in range(n):
             if matrix[row][j] != 0:
@@ -45,9 +46,42 @@ def setZeroes2(matrix):
                 matrix[i][j] = 0
 
     
+def setZeroes3(matrix):
+    """Make first row and first column of matrix as indicator for tracking zeroes 
+    and have one extra variable for column"""
+    m = len(matrix)
+    n = len(matrix[0])
+    col0 = 1
+    for i in range(m):
+        for j in range(n):
+            if matrix[i][j] == 0:
+                matrix[i][0] = 0
+                if j == 0:
+                    col0 = 0
+                else:
+                    matrix[0][j] = 0
+    #Leaving first row and column make other elements as 0
+    print(matrix)
+    for i in range(1,m):
+        for j in range(1,n):
+            if matrix[i][0] == 0 or matrix[0][j] == 0:
+                matrix[i][j] = 0
 
+    #Deal with first row except first element
+    if matrix[0][0] == 0:
+        for i in range(1,n):
+                matrix[0][i] = 0
 
-matrix = [[0,1,2,0],[3,4,5,2],[1,3,1,5]]
+    #Deal with first column
+    for i in range(m-1,0,-1):
+        if col0 == 0:
+            matrix[i][0] = 0
 
-setZeroes2(matrix)
+    print(matrix)
+
+    
+
+matrix = [[0,1,2,0],[3,4,5,2],[1,3,0,5]]
+matrix = [[1,1,1],[1,0,1],[1,1,1]]
+setZeroes3(matrix)
 print(matrix)
